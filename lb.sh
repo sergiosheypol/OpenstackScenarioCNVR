1081b38ca6c587dd6dc3cd631fcf5aebcadd76a3
Sergio López <sergiolf96@gmail.com>
2020-01-02 17:36:07 +0100

    LB commands added. Not tested

---
+++ lb.sh
@@ -0,0 +1,16 @@
+# Creación LB
+neutron lbaas-loadbalancer-create --name subnet1 subnet1
+
+openstack security group create lbaasv2
+openstack security group rule create --protocol icmp --ingress lbaasv2
+openstack security group rule create --protocol tcp --dst-port 80:80 lbaasv2
+
+openstack port set --security-group lbaasv2 d5d89d1e-c47b-4e74-b5d8-9809f46050fb  # Usar grep
+
+neutron lbaas-listener-create --name lb-http --loadbalancer lb --protocol HTTP --protocol-port 80
+
+# Create pool and use Round Bobin
+neutron lbaas-pool-create --name lb-http-pool --lb-algorithm ROUND_ROBIN --listener lb-http --protocol HTTP
+
+# Add members to pool
+neutron lbaas-member-create --name lb-member-01 --subnet subnet1 --address 192.168.100.41 --protocol-port 80 lb-http-pool # usar grep
