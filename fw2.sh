d055202ca77503f8eca40e8d7b57497ee6a67323
SofiaVU <sofia.v.u@hotmail.es>
2020-01-17 16:52:11 +0100

    cloud-init added

--- fw2.sh
+++ fw2.sh
@@ -1,10 +1,9 @@
 # IPs
 LB="$(neutron lbaas-loadbalancer-show lb | awk 'FNR == 14 {print $4}')"
-ADMIN="$(openstack server show admin_server -c addresses -f value | awk 'FNR == 1 {print $2}' | awk -F "=" '{print $2}' | awk -F "," '{print $1}')"
 
 # Rules creation
 openstack firewall group rule create --protocol tcp --destination-port 80 --destination-ip-address "${LB}" --action allow --name fw_rule_lb
-openstack firewall group rule create --protocol tcp --destination-port 22 --destination-ip-address "${ADMIN}" --action allow --name fw_rule_admin
+openstack firewall group rule create --protocol tcp --destination-port 22 --destination-ip-address 10.100.0.10 --action allow --name fw_rule_admin
 openstack firewall group rule create --protocol any --source-ip-address 10.100.0.0/24 --action allow --name fw_rule_int_1
 openstack firewall group rule create --protocol any --source-ip-address 10.200.0.0/24 --action allow --name fw_rule_int_2
 
